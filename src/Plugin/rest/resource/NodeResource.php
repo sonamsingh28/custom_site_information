0414d255b9aebb289d9de0de40dda1189d6bbcab
sonam <sonam.singh@innoraft.com>
2018-04-11 13:15:44 +0530

    Initial commit

---
+++ src/Plugin/rest/resource/NodeResource.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace Drupal\custom_site_information\Plugin\rest\resource;
+
+use Drupal\rest\Plugin\ResourceBase;
+use Drupal\Core\Session\AccountProxyInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Psr\Log\LoggerInterface;
+use Drupal\node\Entity\Node;
+use Drupal\rest\ModifiedResourceResponse;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+
+/**
+ * Provides a resource to get node data in JSON.
+ *
+ * @RestResource(
+ *   id = "node_resource",
+ *   label = @Translation("Node Resource"),
+ *   uri_paths = {
+ *     "canonical" = "/page_json/{apikey}/{nid}"
+ *   }
+ * )
+ */
+class NodeResource extends ResourceBase {
+  /**
+   * A curent user instance.
+   *
+   * @var \Drupal\Core\Session\AccountProxyInterface
+   */
+  protected $currentUser;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->getParameter('serializer.formats'),
+      $container->get('logger.factory')->get('rest'),
+      $container->get('current_user')
+    );
+  }
+
+  /**
+   * Constructs a Drupal\rest\Plugin\ResourceBase object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param array $serializer_formats
+   *   The available serialization formats.
+   * @param \Psr\Log\LoggerInterface $logger
+   *   A logger instance.
+   */
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    array $serializer_formats,
+    LoggerInterface $logger,
+    AccountProxyInterface $current_user) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $serializer_formats, $logger);
+
+    $this->currentUser = $current_user;
+  }
+
+  /**
+   * Responds to GET requests.
+   *
+   * Returns the node data if api key matches.
+   *
+   * @return \Drupal\rest\ResourceResponse
+   *   The response containing node data.
+   *
+   * @throws \Symfony\Component\HttpKernel\Exception\HttpException
+   */
+  public function get($apikey = NULL, $nid = NULL) {
+    // Uses current user after pass authentication to validate access.
+    if (!$this->currentUser->hasPermission('access content')) {
+      throw new AccessDeniedHttpException();
+    }
+    // Fetching the site api key.
+    $required_api_key = \Drupal::config('system.site')->get('siteapikey');
+    // Checking if nid in url is valid and of type page.
+    $values = \Drupal::entityQuery('node')
+      ->condition('nid', $nid)
+      ->condition('type', 'page')
+      ->execute();
+    $node_exists = !empty($values);
+    $data = '';
+    // Checking if api key present in url matches the one saved in siteapikey
+    // variable and whether the nid in url is valid or not.
+    if (!strcmp($required_api_key, $apikey) && $node_exists) {
+      $serializer = \Drupal::service('serializer');
+      $node = Node::load($nid);
+      // Serializing the node data.
+      $data = $serializer->serialize($node, 'json', ['plugin_id' => 'entity']);
+    }
+    else {
+      throw new AccessDeniedHttpException('access denied');
+    }
+
+    return new ModifiedResourceResponse($data);
+  }
+
+}
